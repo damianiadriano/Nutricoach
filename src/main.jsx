7521724e516b2ccae81c4521ea6a8fea10015a59
damianiadriano <damiani.adriano@hotmail.it>
2026-06-13 20:52:04 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom/client";
 import NutriCoach from "./NutriCoach.jsx";
 import "./index.css";
 
+// Registrazione esplicita del service worker (richiesta con injectManifest)
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw-push.js", { scope: "/" })
+      .then(reg => console.log("SW registrato:", reg.scope))
+      .catch(err => console.warn("SW registration failed:", err));
+  });
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <NutriCoach />
